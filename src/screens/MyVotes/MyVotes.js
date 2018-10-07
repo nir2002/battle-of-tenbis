5c36376ffeb6edfbc39ed1e7214446681f75b548
Nir Parisian <nir.parisian@gmail.com>
2018-10-07 21:16:05 +0300

    basic pages

--- src/screens/MyVotes/MyVotes.js
+++ src/screens/MyVotes/MyVotes.js
@@ -1,7 +1,44 @@
 import React from "react";
+import Table from "../../components/Tables/Table";
 
+let id = 0;
+export const createMyRestuarantsVotesData = (name, vote) => {
+  return { id: id++, name, vote };
+};
+
+const data = [
+  createMyRestuarantsVotesData("Yashka", "white"),
+  createMyRestuarantsVotesData("Piazza", "white"),
+  createMyRestuarantsVotesData("Restaurant 3", "black"),
+  createMyRestuarantsVotesData("Restaurant 4", "black"),
+  createMyRestuarantsVotesData("Restaurant 5", "black")
+];
 export default class MyVotes extends React.Component {
   render() {
-    return <div>My Votes Page</div>;
+    return (
+      <section>
+        <h2>My Votes Page</h2>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center"
+          }}
+        >
+          <Table
+            rows={[
+              {
+                label: "Name",
+                value: data => data.name
+              },
+              {
+                label: "Vote",
+                value: data => data.vote
+              }
+            ]}
+            data={data}
+          />
+        </div>
+      </section>
+    );
   }
 }
--- src/screens/Restaurants/Restaurants.js
+++ src/screens/Restaurants/Restaurants.js
@@ -1,7 +1,67 @@
 import React from "react";
+import Table from "../../components/Tables/Table";
+
+let id = 0;
+export const createRestuarantsData = (name, voters) => {
+  return { id: id++, name, ...voters };
+};
+
+const data = [
+  createRestuarantsData("Yashka", [{ name: "Nir" }, { name: "Dan" }]),
+  createRestuarantsData("Piazza", [{ name: "Ben" }, { name: "Dan" }]),
+  createRestuarantsData("Restaurant 3", [
+    { name: "Ben" },
+    { name: "Avi" },
+    { name: "Alon" }
+  ]),
+  createRestuarantsData("Restaurant 4", [
+    { name: "Naor" },
+    { name: "Nir" },
+    { name: "Lev" }
+  ]),
+  createRestuarantsData("Restaurant 5", [
+    { name: "Eyal" },
+    { name: "Lev" },
+    { name: "Avi" }
+  ])
+];
 
 export default class Restaurants extends React.Component {
   render() {
-    return <div>Resatuarnts Page</div>;
+    return (
+      <div>
+        <h1>Resatuarnts Page</h1>
+        <div style={{ display: "flex", justifyContent: "space-around" }}>
+          <Table
+            color="white"
+            rows={[
+              {
+                label: "Name",
+                value: data => data.name
+              },
+              {
+                label: "Voters",
+                value: data => data.voters
+              }
+            ]}
+            data={data}
+          />
+          <Table
+            color="black"
+            rows={[
+              {
+                label: "Name",
+                value: data => data.name
+              },
+              {
+                label: "Voters",
+                value: data => data.voters
+              }
+            ]}
+            data={data}
+          />
+        </div>
+      </div>
+    );
   }
 }
