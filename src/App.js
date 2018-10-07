594459c2adc0762360c0a587bc27efe9ec97c130
Nir Parisian <nir.parisian@gmail.com>
2018-10-08 00:02:54 +0300

    added app bar to the application

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
 import Restaurants from "./screens/Restaurants";
 import MyVotes from "./screens/MyVotes";
+import AppBar from "./components/AppBar";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The battle of TenBis</h1>
+        <AppBar />
         <Switch>
           <Route exact path="/restaurants" component={Restaurants} />
           <Route exact path="/my-votes" component={MyVotes} />
