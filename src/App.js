98ab764abe740e6be7df116e69d760cb1289611b
Nir Parisian <nir.parisian@gmail.com>
2018-10-07 21:15:26 +0300

    app routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Restaurants from "./screens/Restaurants";
 import MyVotes from "./screens/MyVotes";
 import "./App.css";
@@ -8,9 +8,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route path="/restaurants" component={Restaurants} />
-        <Route path="/my-votes" component={MyVotes} />
-        <Redirect from="/" to="/restaurants" />
+        <h1>The battle of TenBis</h1>
+        <Switch>
+          <Route exact path="/restaurants" component={Restaurants} />
+          <Route exact path="/my-votes" component={MyVotes} />
+          <Redirect from="/" to="/restaurants" />
+        </Switch>
       </div>
     );
   }
